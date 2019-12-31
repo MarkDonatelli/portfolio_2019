0e2f1ffe6545ec8111a847ef346fdba609f95464
Mark Donatelli <markwdonatelli@yahoo.com>
2019-12-31 16:33:27 -0500

    style updates

--- src/js/app.js
+++ src/js/app.js
@@ -1,68 +1,76 @@
-const navButton = document.querySelector('.menu-icon');
-const navButtonClose = document.querySelector('.menu-icon-close');
+const navButton = document.querySelector(".menu-icon");
+const navButtonClose = document.querySelector(".menu-icon-close");
 
 const tl = new TimelineLite({ paused: true });
 
-tl.to('.menu-icon', 1, {
+tl.to(".menu-icon", 1, {
   x: 500,
   opacity: 0,
-  ease: Power4.easeOut,
+  ease: Power4.easeOut
 })
   .to(
-    '.main-menu',
+    ".main-menu",
     1,
     {
       top: 0,
-      ease: Power4.easeOut,
+      ease: Power4.easeOut
     },
-    '-=1'
+    "-=1"
   )
   .to(
-    '.one',
+    ".one",
     1,
     {
       opacity: 1,
       top: 0,
-      ease: Power4.easeOut,
+      ease: Power4.easeOut
     },
-    '0.5'
+    "0.5"
   )
   .to(
-    '.two',
+    ".two",
     1,
     {
       opacity: 1,
       top: 0,
-      ease: Power4.easeOut,
+      ease: Power4.easeOut
     },
-    '0.7'
+    "0.7"
   )
   .to(
-    '.three',
+    ".three",
     1,
     {
       opacity: 1,
       top: 0,
-      ease: Power4.easeOut,
+      ease: Power4.easeOut
     },
-    '0.9'
+    "0.9"
   )
   .to(
-    '.four',
+    ".four",
     1,
     {
       opacity: 1,
       top: 0,
-      ease: Power4.easeOut,
+      ease: Power4.easeOut
     },
-    '1.1'
+    "1.1"
   );
 
-navButton.addEventListener('click', () => {
+navButton.addEventListener("click", () => {
   tl.play();
 });
 
-navButtonClose.addEventListener('click', () => {
+navButtonClose.addEventListener("click", () => {
   tl.reverse();
 });
 
+var animateIn = new TimelineMax();
+
+animateIn.fromTo(
+  ".overlay-animation",
+  2,
+  { skewX: 30, scale: 1.5 },
+  { skewX: 0, xPercent: 100, transformOrigin: "0% 100%", ease: Power4.easeOut }
+);
